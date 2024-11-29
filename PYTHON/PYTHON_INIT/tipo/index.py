915fbf87cd79327af79ad404d18b279b362a31e3
Adam Bastos Santos <38339190+adambastos@users.noreply.github.com>
2024-11-28 23:38:51 -0300

    Python for Begginers 2

--- PYTHON/PYTHON_INIT/tipo/index.py
+++ PYTHON/PYTHON_INIT/tipo/index.py
@@ -1,10 +1,6 @@
-"""
+
 nasc = int(input("Digite seu ano de nascimento: "))
 idade = 2024 - nasc
 
-print("Você tem " + str(idade) + " anos") """ 
+print("Você tem " + str(idade) + " anos") 
 
-num = input(
-    "Float? "
-)
-print("Opa " + num)
---
+++ PYTHON/PYTHON_INIT/tipo/lists.py
@@ -0,0 +1,9 @@
+names = ["Adam", "Bastos", "Santos"]
+
+for name in names: #Para cada name em names, imprima name.
+    print(name)
+
+numbers = [1, 2, 3, 4, 5, 6]
+numbers.insert(0, -1) #Insira o número -1 na posição 0 do vetor.
+numbers.remove(4) #Remove o número 4 da lista, e não o elemento na quarta posição
+print(numbers)
